0c86bb70c38a94c4052325a413cee5969fffd16b
Anastazja Nikola Bokun <anastazjabokun@tlen.pl>
2023-04-20 00:39:56 +0200

    homework almost done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,14 @@
-export { fetchCountries };
+export { fetchCountries }
 
-function fetchCountries() {
-    return fetch(
-      "https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name"
-    ).then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-  };
+
+function fetchCountries(query) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${query}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Notiflix.Notify.failure('Qui timide rogat docet negare')(response.status);
+    }
+    return response.json();
+  });
+}
+
--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
 import './css/styles.css';
-import { fetchCountries } from "./fetchCountries";
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+
 
 const fetchCountriesInput = document.querySelector("#search-box");
-
-fetchCountriesInput.addEventListener("input", () => {
-  fetchCountries()
-    .then((countries) => renderUserList(countries))
-    .catch((error) => console.log(error));
-});
+const countryList = document.querySelector(".country-list");
+const countrySingle = document.querySelector(".country-info");
+var debounce = require('lodash.debounce');
 
 
+fetchCountriesInput.addEventListener("input", debounce(() => {
+  const query = fetchCountriesInput.value;
+  fetchCountries(query.trim())
+    .then((countries) => renderCountryList(countries))
+    .catch((error) => Notiflix.Notify.failure('Oops, there is no country with that name'));
+}, 300));
 
-function renderUserList(countries) {
-  const markup = countries
-    .map((user) => {
+function renderCountryList(countries) {
+  const countriesArray = countries
+    .map((country) => {
+      return country.name.official
+    })
+    const countryName = countries
+    .map((country) => {
       return `
           <li>
-            <p><b>Name</b>: ${user.name}</p>
-            <p><b>Email</b>: ${user.email}</p>
-            <p><b>Company</b>: ${user.company.name}</p>
+          <img src=${country.flags.svg} alt="flag" width="60" /><p>${country.name.official}</p>
+          </li>
       `;
     })
     .join("");
-  console.log(markup);
-}
+    const countryDetails = countries
+    .map((country) => {
+      const languageCodes = Object.keys(country.languages);
+      const languages = languageCodes.map((code) => country.languages[code]);
+      return `
+          <img src=${country.flags.svg} alt="flag" width="60" /><p>${country.name.official}</p>
+          <p><b>Capital</b>: ${country.capital}</p>
+          <p><b>Population</b>: ${country.population}</p>
+          <p><b>Languages</b>: ${languages.join(', ')}</p>
+      `;
+    })
+    .join("");
 
+if (countriesArray.length > 9) {
+  Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+  countryList.innerHTML = " ";
+  countrySingle.innerHTML = " ";
+} else if (countriesArray.length > 1) {
+countryList.innerHTML = countryName;
+countrySingle.innerHTML = "";
+} else if (countriesArray.length = 1) {
+  countrySingle.innerHTML = countryDetails;
+  countryList.innerHTML = "";
+} 
+}
 
 const DEBOUNCE_DELAY = 300;
