1e24fadc203768448207afb9835373e07832567e
Anastazja Nikola Bokun <anastazjabokun@tlen.pl>
2023-04-28 00:23:10 +0200

    homework another part

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('form');
 const gallery = document.querySelector('.gallery');
+const localStorageKey = "key";
 
 const axios = require('axios').default;
-
-console.log(form);
+let pageNumber = 1;
 
 
 async function getUser(event) {
@@ -14,17 +14,19 @@ async function getUser(event) {
     let {
         elements: { searchQuery },
       } = event.currentTarget;
-      const query = searchQuery.value;
+      const query = searchQuery.value
     {
     try {
-      const response = await axios.get(`https://pixabay.com/api/?key=35771389-a0cc59dab35b925a54e6666bb&q=${query}&image_type=photo&orienation=horizontal&safesearch=true&per_page=40`);
-renderPictureGrid(response);
+      const response = await axios.get(`https://pixabay.com/api/?key=35771389-a0cc59dab35b925a54e6666bb&q=${query}&image_type=photo&orienation=horizontal&safesearch=true&per_page=40&page=${pageNumber}`);
+renderPictureGrid(response)
+pageNumber += 1;
     } catch (error) {
       console.error(error);
       Notiflix.Notify.failure('Oops, something went wrong')
     }
   }
-};
+
+  localStorage.setItem(localStorageKey, query);
 
 function renderPictureGrid(response) {
     const pictures = response.data.hits;
@@ -55,66 +57,15 @@ function renderPictureGrid(response) {
       </div>`
       })
       .join("");
-gallery.innerHTML = grid;
-    }
+      if (query !== localStorage.getItem(localStorageKey)) {
+        gallery.innerHTML = grid;
+
+      } else {
+        gallery.insertAdjacentHTML('beforeend', grid);
+      }
   }
+}
+};
 form.addEventListener('submit', getUser)
 
 
-
-// const fetchCountriesInput = document.querySelector("#search-box");
-// const countryList = document.querySelector(".country-list");
-// const countrySingle = document.querySelector(".country-info");
-// var debounce = require('lodash.debounce');
-
-
-// fetchCountriesInput.addEventListener("input", debounce(() => {
-//     const query = fetchCountriesInput.value;
-//   fetchCountries(query.trim())
-//     .then((countries) => renderCountryList(countries))
-//     .catch((error) => Notiflix.Notify.failure('Oops, there is no country with that name'));   
-//     countryList.innerHTML = "";
-//     countrySingle.innerHTML = "";
-// }, 300));
-
-// function renderCountryList(countries) {
-//   const countriesArray = countries
-//     .map((country) => {
-//       return country.name.official
-//     })
-//     const countryName = countries
-//     .map((country) => {
-//       return `
-//           <li>
-//           <img src=${country.flags.svg} alt="flag" height="20" /><p>${country.name.official}</p>
-//           </li>
-//       `;
-//     })
-//     .join("");
-//     const countryDetails = countries
-//     .map((country) => {
-//       const languageCodes = Object.keys(country.languages);
-//       const languages = languageCodes.map((code) => country.languages[code]);
-//       return `
-//           <img src=${country.flags.svg} alt="flag" width="60" /><p>${country.name.official}</p>
-//           <p><b>Capital</b>: ${country.capital}</p>
-//           <p><b>Population</b>: ${country.population}</p>
-//           <p><b>Languages</b>: ${languages.join(', ')}</p>
-//       `;
-//     })
-//     .join("");
-
-// if (countriesArray.length > 9) {
-//   Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-//   countryList.innerHTML = "";
-//   countrySingle.innerHTML = "";
-// } else if (countriesArray.length > 1) {
-// countryList.innerHTML = countryName;
-// countrySingle.innerHTML = "";
-// } else if (countriesArray.length = 1) {
-//   countrySingle.innerHTML = countryDetails;
-//   countryList.innerHTML = "";
-// } 
-// }
-
-// const DEBOUNCE_DELAY = 300;
